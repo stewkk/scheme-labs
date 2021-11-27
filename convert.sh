ea790d061898c254bea4293c14f548cd31e7ea11
Starovoytov Alexandr <saniy200311@gmail.com>
2021-11-27 16:43:24 +0300

    move org files to solutions dir

--- convert.sh
+++ convert.sh
@@ -3,8 +3,12 @@
 # конвертирует каждый *.md файл в *.org,
 # если файл с таким именем еще не существует
 
-for file in ./*.md; do
-    if [ ! -e "${file%.*}.org" ]; then
-        pandoc -so "${file%.*}.org" "$file"
+if [ ! -e "solutions/" ]; then
+    mkdir "solutions"
+fi
+
+for file in *.md; do
+    if [ ! -e "solutions/${file%.*}.org" ]; then
+        pandoc -so "solutions/${file%.*}.org" "$file"
     fi
 done
