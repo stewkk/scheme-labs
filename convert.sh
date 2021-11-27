1547b54adf3c25445a2a5377b743bfea77278272
Starovoytov Alexandr <saniy200311@gmail.com>
2021-11-27 21:02:26 +0300

    move org files to root

--- convert.sh
+++ convert.sh
@@ -3,12 +3,8 @@
 # конвертирует каждый *.md файл в *.org,
 # если файл с таким именем еще не существует
 
-if [ ! -e "solutions/" ]; then
-    mkdir "solutions"
-fi
-
-for file in *.md; do
-    if [ ! -e "solutions/${file%.*}.org" ]; then
-        pandoc -so "solutions/${file%.*}.org" "$file"
+for file in ./*.md; do
+    if [ ! -e "${file%.*}.org" ]; then
+        pandoc -so "${file%.*}.org" "$file"
     fi
 done
